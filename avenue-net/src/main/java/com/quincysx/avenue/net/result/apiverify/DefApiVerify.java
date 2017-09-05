8da8757b5372da70d1482a894a331364adc6eb0d
QuincySx <772804430@qq.com>
2017-09-05 14:34:47 +0800

    添加版权信息

--- avenue-net/src/main/java/com/quincysx/avenue/net/result/apiverify/DefApiVerify.java
+++ avenue-net/src/main/java/com/quincysx/avenue/net/result/apiverify/DefApiVerify.java
@@ -1,4 +1,19 @@
 package com.quincysx.avenue.net.result.apiverify;
+/*
+ * Copyright (C) 2017 QuincySx<quincysx@sina.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import com.quincysx.avenue.net.result.exception.ApiException;
 
@@ -18,6 +33,6 @@ public final class DefApiVerify implements IApiVerify {
 
     @Override
     public synchronized <T> void verify(T t) throws ApiException {
-        
+
     }
 }
